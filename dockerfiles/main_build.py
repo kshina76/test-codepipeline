292d1338fa29fed20a55dad5b6d407484889c0f3
kshina76 <koushina47@gmail.com>
2020-02-01 09:27:08 +0000

    add

--- dockerfiles/main_build.py
+++ dockerfiles/main_build.py
@@ -2,34 +2,34 @@ import shlex
 import subprocess
 import os
 
-# シェルコマンドを実行する
+# execute shell command
 def exec_cmd(cmd):
     tokens = shlex.split(cmd)
     subprocess.run(tokens)
 
-# コマンドの実行結果が返り値になる。コマンドの返り値がコマンドになるものに使う。
-# 注意点は改行コードを削除すること
+# using for return command from command
+# delete new line
 def get_cmd_stdout(cmd):
     tokens = shlex.split(cmd)
     tokens = subprocess.run(tokens, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return tokens.stdout.decode("utf8").replace('\n','')
 
-# リポジトリ名を取得
+# get repository name
 REPO_APP = 'aws ecr describe-repositories --repository-names app --output text --query "repositories[0].repositoryUri"'
 REPO_APP = get_cmd_stdout(REPO_APP)
 REPO_WEB = 'aws ecr describe-repositories --repository-names web --output text --query "repositories[0].repositoryUri"'
 REPO_WEB = get_cmd_stdout(REPO_WEB)
 
 if __name__ == "__main__":
-    # flask+uwsgi+pythonのDockerfileをビルド
+    # build flask,uwsgi and python that is Dockerfile
     build_app = 'docker build -t {}:latest ./app/'.format(REPO_APP)
     exec_cmd(build_app)
 
-    # nginxのDockerfileをビルド
+    # nginx that is Dockerfile
     build_web = 'docker build -t {}:latest ./nginx/'.format(REPO_WEB)
     exec_cmd(build_web)
 
-    # イメージをpush
+    # pushing image to ecr
     for repo in [REPO_APP, REPO_WEB]:
         push_image = 'docker push {}:latest'.format(repo)
         exec_cmd(push_image)
--- dockerfiles/pre_build.py
+++ dockerfiles/pre_build.py
@@ -2,19 +2,19 @@ import shlex
 import subprocess
 import os
 
-# シェルコマンドを実行する
+# execute shell command
 def exec_cmd(cmd):
     tokens = shlex.split(cmd)
     subprocess.run(tokens)
 
-# コマンドの実行結果が返り値になる。コマンドの返り値がコマンドになるものに使う
+# using for return command from command
 def get_cmd_stdout(cmd):
     tokens = shlex.split(cmd)
     tokens = subprocess.run(tokens, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return tokens.stdout.decode("utf8")
 
 
-# ecrにログイン
+# login to ecr
 LOGIN_TO_ECR = 'aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email'
 # LOGIN_TO_ECR = 'aws ecr get-login --no-include-email --region ap-northeast-1'
 LOGIN_TO_ECR = get_cmd_stdout(LOGIN_TO_ECR)
